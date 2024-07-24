f65da23d03398c502435b11132839922c4547974
AviTirto <avi.tirto@gmail.com>
2024-07-24 16:24:25 +0700

    Gemini Speech-to-Speech Model

--- VirtualAssistant/server/main.py
+++ VirtualAssistant/server/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, UploadFile, Depends, File
 from fastapi.responses import StreamingResponse
 from openai import AzureOpenAI
 from dotenv import load_dotenv
+import vertexai
+from google.oauth2 import service_account
+from vertexai.generative_models import GenerativeModel, Part
+import base64
 import os
 from io import BytesIO
 import time
@@ -9,7 +13,7 @@ import time
 app = FastAPI()
 load_dotenv()
 
-def getClient(api_key, api_version, azure_endpoint):
+def getAzureClient(api_key, api_version, azure_endpoint):
     client = AzureOpenAI(
         api_key= os.getenv(api_key),  
         api_version=os.getenv(api_version),
@@ -19,10 +23,12 @@ def getClient(api_key, api_version, azure_endpoint):
 
 def getClients():
     return {
-        "whisper_client": getClient("AZUREOPENAI_SPEECHMODEL", "TRANSCRIPTIONMODEL_VERSION", "AZUREOPENAI_SPEECHMODEL_ENDPOINT"),
-        "chat_client": getClient("AZUREOPENAI_CHATMODEL", "CHATMODEL_VERSION", "AZUREOPENAI_CHATMODEL_ENDPOINT"),
-        "tts_client": getClient("AZUREOPENAI_SPEECHMODEL", "TTSMODEL_VERSION", "AZUREOPENAI_SPEECHMODEL_ENDPOINT"),
+        "whisper_client": getAzureClient("AZUREOPENAI_SPEECHMODEL", "TRANSCRIPTIONMODEL_VERSION", "AZUREOPENAI_SPEECHMODEL_ENDPOINT"),
+        "chat_client": getAzureClient("AZUREOPENAI_CHATMODEL", "CHATMODEL_VERSION", "AZUREOPENAI_CHATMODEL_ENDPOINT"),
+        "tts_client": getAzureClient("AZUREOPENAI_SPEECHMODEL", "TTSMODEL_VERSION", "AZUREOPENAI_SPEECHMODEL_ENDPOINT"),
     }
+
+# Azure OpenAI Speech to Speech Methods:
 
 def transcribe_audio(audio_file, whisper_client):
     
@@ -49,9 +55,6 @@ def generate_response(text: str, chat_client):
         )
     return response.choices[0].message.content
 
-def audio_to_gen_response(audio, chat_client):
-    pass
-
 def TTS(text: str, tts_client):
     response = tts_client.audio.speech.create(
         model=os.getenv("TTS_DEPLOYMENT_MODEL"),
@@ -60,6 +63,36 @@ def TTS(text: str, tts_client):
     )
     audio_stream = BytesIO(response.content)
     return StreamingResponse(audio_stream, media_type="audio/mpeg", headers={"Content-Disposition": "attachment; filename=speech.mp3"})
+
+# Gemini-Flash Speech to Speech Methods:
+
+def audio_to_gen_response(audio):
+    prompt = """
+    You work for mandiri sekuritas. We just released our new trading platform called growin.
+    Growin offers a comprehensive range of features for stock trading, mutual fund investment, and commodities trading.
+    Growin offers an investment experience to you, that is now better, easier, and more convenient.
+    Listen to the audio clip and answer the question.
+    """
+
+    credentials = service_account.Credentials.from_service_account_file(os.getenv("GEMINI_CREDENTIALS"))
+    vertexai.init(project="mansek-data", location="us-central1", credentials=credentials)
+    
+    model = GenerativeModel(
+        "gemini-1.5-pro-001",
+    )
+
+    audio_file = Part.from_data(
+        mime_type="audio/mpeg",
+        data=audio
+    )
+
+    responses = model.generate_content(
+        [prompt, audio_file]
+    )
+
+    print(responses.text)
+
+    return responses.text
 
 @app.post("/talk")
 async def talk(file: UploadFile = File(...), clients:getClients = Depends()):
@@ -90,13 +123,13 @@ async def talk(file: UploadFile = File(...), clients:getClients = Depends()):
 async def talk(file: UploadFile = File(...), clients:getClients = Depends()):
     start = time.time()
     audio = await file.read()
-    buffer = BytesIO(audio)
-    buffer.name = 'audio.m4a'
+    buffer = base64.b64encode(audio)
+    buffer = buffer.decode('utf-8')
     end = time.time()
     print("Parsing Input File: ", end-start)
     
     start = time.time()
-    response = generate_response("...", clients["chat_client"])
+    response = audio_to_gen_response(buffer)
     end = time.time()
     print("ChatGPT Conversation: ", end-start)
 
