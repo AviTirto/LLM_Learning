9021912115f1cee7e0004b4a272a90d4b22e6c28
AviTirto <avi.tirto@gmail.com>
2024-07-24 13:29:27 +0700

    Speech to Speech using Azure Open AI

--- VirtualAssistant/server/main.py
+++ VirtualAssistant/server/main.py
@@ -1,24 +1,26 @@
 from fastapi import FastAPI, UploadFile, Depends, File
+from fastapi.responses import StreamingResponse
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 import os
-import io
+from io import BytesIO
 
 app = FastAPI()
 load_dotenv()
 
-def getClient(api_key, azure_endpoint):
+def getClient(api_key, api_version, azure_endpoint):
     client = AzureOpenAI(
         api_key= os.getenv(api_key),  
-        api_version="2024-02-01",
+        api_version=os.getenv(api_version),
         azure_endpoint = os.getenv(azure_endpoint)
     )
     return client
 
 def getClients():
     return {
-        "whisper_client": getClient("AZUREOPENAI_WHISPERMODEL", "AZUREOPENAI_WHISPERMODEL_ENDPOINT"),
-        "chat_client": getClient("AZUREOPENAI_CHATMODEL", "AZUREOPENAI_CHATMODEL_ENDPOINT"),
+        "whisper_client": getClient("AZUREOPENAI_SPEECHMODEL", "TRANSCRIPTIONMODEL_VERSION", "AZUREOPENAI_SPEECHMODEL_ENDPOINT"),
+        "chat_client": getClient("AZUREOPENAI_CHATMODEL", "CHATMODEL_VERSION", "AZUREOPENAI_CHATMODEL_ENDPOINT"),
+        "tts_client": getClient("AZUREOPENAI_SPEECHMODEL", "TTSMODEL_VERSION", "AZUREOPENAI_SPEECHMODEL_ENDPOINT"),
     }
 
 def transcribe_audio(audio_file, whisper_client):
@@ -34,7 +36,7 @@ def generate_response(text: str, chat_client):
     response = chat_client.chat.completions.create(
             model=os.getenv("CHATMODEL_DEPLOYMENT_MODEL"),
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "system", "content": "You work for mandiri sekuritas. We just released our new trading platform called growin."},
                 {"role": "user", "content": f"{text}"},
             ],
             temperature=0.2,
@@ -42,15 +44,21 @@ def generate_response(text: str, chat_client):
         )
     return response.choices[0].message.content
 
+def TTS(text: str, tts_client):
+    response = tts_client.audio.speech.create(
+        model=os.getenv("TTS_DEPLOYMENT_MODEL"),
+        voice="alloy",
+        input=text
+    )
+    audio_stream = BytesIO(response.content)
+    return StreamingResponse(audio_stream, media_type="audio/mpeg", headers={"Content-Disposition": "attachment; filename=speech.mp3"})
+
 @app.post("/talk")
 async def talk(file: UploadFile = File(...), clients:getClients = Depends()):
     audio = await file.read()
-    buffer = io.BytesIO(audio)
+    buffer = BytesIO(audio)
     buffer.name = 'audio.m4a'
     
     transcription = transcribe_audio(buffer, clients["whisper_client"])
-    print(transcription)
     response = generate_response(transcription.text, clients["chat_client"])
-    print(response)
-
-    return response
+    return TTS(response, clients["tts_client"])
